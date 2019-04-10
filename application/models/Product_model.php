d6c638b387201cef2278020f2e85ad650e4dfeb6
Marc Lorenz Comia <marclorenz27@yahoo.com>
2019-04-10 09:17:40 +0800

    Updated Product_model.php

--- application/models/Product_model.php
+++ application/models/Product_model.php
@@ -4,28 +4,6 @@
 			$this->load->database();
 		}
 
-		// public function get_products($slug = FALSE){
-		// 	if($slug === FALSE){
-		// 		$this->db->select_avg('appraised_amount', 'average_per_group');
-		// 		$this->db->select('products.*'); //SELECT * FROM products
-		// 		$this->db->select('categories.*');
-		// 		$this->db->select('brands.*');
-		// 		$this->db->select('product_photo.*');
-		// 		$this->db->join('categories', 'categories.category_id = products.category_id');
-		// 		$this->db->join('brands', 'brands.brand_id = products.brand_id','left');
-		// 		$this->db->join('product_photo', 'product_photo.photo_id = products.photo_id');
-		// 		$this->db->group_by('product_name');
- 	// 			$query = $this->db->get('products');
-		// 		return $query->result_array(); // to return multiple records / for index
-		// 	}
-
-		// 	$this->db->join('categories', 'categories.category_id = products.category_id');
-		// 	$this->db->join('brands', 'brands.brand_id = products.brand_id','left');
-		// 	$this->db->join('product_photo', 'product_photo.photo_id = products.photo_id');
-		// 	$query = $this->db->get_where('products', array('slug' => $slug));
-		// 	return $query->row_array(); // to return 1.
-		// }
-
 		public function get_products($slug = FALSE){
 			if($slug === FALSE){
 				$this->db->select_avg('appraised_amount', 'average_per_group');
